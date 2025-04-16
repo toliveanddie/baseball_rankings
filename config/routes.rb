252fa7b68ff595c2ea99b3022119b349e12a3926
Norm <toliveanddie@gmail.com>
2025-04-16 10:58:05 -0400

    add scaffolded models for teams and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :teams
   root "home#overall"
   get "home/weekly", to: "home#weekly"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
